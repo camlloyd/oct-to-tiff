aa795b605edc09d88441915fe299da6368acb076
Cameron Lloyd <lloyd@med.unideb.hu>
2021-09-26 22:25:41 +0100

    Add support for Angle scan pattern

--- main.py
+++ main.py
@@ -35,6 +35,15 @@ def convert_oct_file():
             pixel_size_x = 0.018182
             pixel_size_y = 0.003071
             pixel_size_z = 0.050000
+        elif 'Angle' in file_name:
+            volume = np.frombuffer(f.read(), dtype=single)
+            frames_per_data_group = 1
+            total_data_groups = 2
+            oct_window_height = 768
+            xy_scan_length = 1020
+            pixel_size_x = 0.002941
+            pixel_size_y = 0.003071
+            pixel_size_z = 1
         elif 'Cornea Cross Line' in file_name:
             volume = np.frombuffer(f.read(), dtype=single)
             frames_per_data_group = 2
