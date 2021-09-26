8edb50bf949a07f7cecf32d9744b4dd3b130f55b
Cameron Lloyd <lloyd@med.unideb.hu>
2021-09-27 00:27:04 +0100

    Update CLI help

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import tifffile as tifffile
 from numpy import single, uint16
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description='Convert optical coherence tomography (.OCT) files to TIFF.')
-parser.add_argument('input', type=str, help='.OCT file to convert')
+parser = argparse.ArgumentParser(description='Convert optical coherence tomography files to OME-TIFF.')
+parser.add_argument('input', type=str, help='OCT file to convert')
 parser.add_argument('--overwrite', default=False, action='store_true', help='overwrite output file if it exists')
 args = parser.parse_args()
 
